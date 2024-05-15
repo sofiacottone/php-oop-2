12f4849fdc67cea8cf169ca64b2ca4d62e3a21e6
sofiacottone <cottonesofia@outlook.it>
2024-05-15 15:53:22 +0200

    feat: add products class + functions

---
+++ Models/Product.php
@@ -0,0 +1,22 @@
+<?php
+class Product
+{
+    public $title;
+    private $price;
+    public $image;
+    public $category = 'Cats and Dogs';
+    public $type;
+
+    public function __construct($_title, $_price, $_image, $_type)
+    {
+        $this->title = $_title;
+        $this->price = $_price;
+        $this->image = $_image;
+        $this->type = $_type;
+    }
+
+    public function getPrice()
+    {
+        return $this->price;
+    }
+}
