01a77046254bce58c129f105dc19eebd349e4ada
sofiacottone <cottonesofia@outlook.it>
2024-05-16 15:08:34 +0200

    feat: add category to products + import files

--- db.php
+++ db.php
@@ -1,12 +1,15 @@
 <?php
 require_once __DIR__ . '/Models/Product.php';
 require_once __DIR__ . '/Models/Category.php';
+require_once __DIR__ . '/Models/Food.php';
+require_once __DIR__ . '/Models/Game.php';
+require_once __DIR__ . '/Models/Bed.php';
 
 // products
-$bed = new Product('Bed', 29.99, 'https://www.petfoodnmore.com/wp-content/uploads/Midwest/Bed/Taupe-cuddle-bed-medium-lg-1024x1024.png', 'Beds');
-$ball = new Product('Beach Balls', 2.99, 'https://www.petfoodnmore.com/wp-content/uploads/2023/08/80531____NCLCAT.jpg', 'Toys');
-$harness = new Product('Harness Blue', 21.99, 'https://www.petfoodnmore.com/wp-content/uploads/2024/03/16383____BLLPXS.jpg', 'Accessories');
-$tuna_dinner = new Product('Tuna Dinner', 1.99, 'https://www.petfoodnmore.com/wp-content/uploads/2021/12/snappy-tom-lites-tuna-cat-wet-food.png', 'Food');
+$bed = new Bed('Bed', 29.99, 'https://www.petfoodnmore.com/wp-content/uploads/Midwest/Bed/Taupe-cuddle-bed-medium-lg-1024x1024.png', $dog, 'XL');
+$ball = new Game('Beach Balls', 2.99, 'https://www.petfoodnmore.com/wp-content/uploads/2023/08/80531____NCLCAT.jpg', $cat);
+$harness = new Accessory('Harness Blue', 21.99, 'https://www.petfoodnmore.com/wp-content/uploads/2024/03/16383____BLLPXS.jpg', $cats_dogs);
+$tuna_dinner = new Food('Tuna Dinner', 1.99, 'https://www.petfoodnmore.com/wp-content/uploads/2021/12/snappy-tom-lites-tuna-cat-wet-food.png', $cat, 85, 'Tuna');
 
 $products = [
     $bed,
